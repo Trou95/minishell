9af6de9401aadb6862676ca740e19847a26aafad
Gorkem95 <gorkem_095@hotmail.com>
2022-09-27 00:46:51 +0300

    Update clear qoutes

--- parser/utils/strutils/ft_clearquotes.c
+++ parser/utils/strutils/ft_clearquotes.c
@@ -8,23 +8,23 @@ char	*ft_str_clearquotes(const char *str, char *ptr)
 	ft_memset(&v, 0, sizeof(t_redir_var));
 	while (str[v.idx])
 	{
-		if (str[v.idx] == '"' || str[v.idx] == '\'' && \
-			(v.idx == 0 || (v.idx > 0 && str[v.idx - 1] != '\\')))
+		if (str[v.idx] == '"' || str[v.idx] == '\'')
 		{
 			c = str[v.idx];
-			while (str[++v.idx] && str[v.idx] != c)
-			{
-				if (str[v.idx] == '\\')
-					v.idx++;
-				ptr[v.n_idx++] = str[v.idx];
-			}
+			v.k_idx = ft_get_chrindex(&str[v.idx + 1],str[v.idx]) + 1;
+			if(str[v.k_idx])
+				if(ft_strnstr(&str[v.idx]," ",v.k_idx))
+				{
+					ft_memcpy(&ptr[v.n_idx],&str[v.idx],v.k_idx + 1);
+					v.n_idx += v.k_idx + 1;
+					v.idx += v.k_idx + 1;
+				}
+			else
+				while (str[++v.idx] && str[v.idx] != c)
+					ptr[v.n_idx++] = str[v.idx];
 		}
 		else
-		{
-			if (str[v.idx] == '\\')
-				v.idx++;
 			ptr[v.n_idx++] = str[v.idx++];
-		}
 	}
 	return (ptr);
 }
